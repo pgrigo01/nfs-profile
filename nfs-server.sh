955a49e3270025e78f97b261887f32ac3cb36f2e
pgrigo01 <pgrigo01@ucy.ac.cy>
2025-03-21 18:22:49 +0200

    updated nfs

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 #
-# Setup a simple FreeBSD NFS server on /nfs
+# Setup an NFS server on /nfs with support for external access from other experiments
 #
 # This script is derived from Jonathan Ellithorpe's Cloudlab profile at
 # https://github.com/jdellithorpe/cloudlab-generic-profile. Thanks!
 #
-# Hacked by mike to work on FreeBSD. The whole strategy has been changed
-# however. Rather than insert commands/variables into the standard system
-# files to have every thing restart on reboot via the standard mechanisms,
-# we handle all the startup from this script. This is because the standard
-# mechanisms run well before the Emulab scripts have configured the
-# experimental LAN we are serving files on. On the other hand, this script
-# runs at the end of the Emulab scripts. I do not know how the old method
-# worked even on Linux when there was a reboot!
-#
+
+# Process command-line arguments
+EXTERNAL_ACCESS="no"
+ALLOWED_NETWORKS="10.0.0.0/8 172.16.0.0/12 192.168.0.0/16"
+
+for arg in "$@"; do
+  case $arg in
+    --external-access=*)
+      EXTERNAL_ACCESS="${arg#*=}"
+      shift
+      ;;
+    --allowed-networks=*)
+      ALLOWED_NETWORKS="${arg#*=}"
+      shift
+      ;;
+    *)
+      # Unknown option
+      ;;
+  esac
+done
+
 . /etc/emulab/paths.sh
 
 OS=$(uname -s)
@@ -49,11 +61,38 @@ if [ "$OS" = "Linux" ]; then
     apt-get update
     stat=`dpkg-query -W -f '${DB:Status-Status}\n' nfs-kernel-server`
     if [ "$stat" = "not-installed" ]; then
-	echo ""
-	echo "Installing NFS packages"
-	apt-get --assume-yes install nfs-kernel-server nfs-common
-	# make sure the server is not running til we fix up exports
-	service nfs-kernel-server stop
+        echo ""
+        echo "Installing NFS packages"
+        apt-get --assume-yes install nfs-kernel-server nfs-common
+        # Install firewall for external access
+        if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+            apt-get --assume-yes install ufw
+        fi
+        # make sure the server is not running til we fix up exports
+        service nfs-kernel-server stop
+    fi
+fi
+
+# Get internal NFS network information
+NFSIP=`grep -i $HOSTNAME-$NFSNETNAME /etc/hosts | awk '{print $1}'`
+NFSNET=`echo $NFSIP | awk -F. '{printf "%s.%s.%s.0", $1, $2, $3}'`
+
+# Get public/external IP if external access is enabled
+PUBLIC_IP=""
+if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+    if [ "$OS" = "Linux" ]; then
+        # Get the public interface (usually eth0 or ens3)
+        PUBLIC_IFACE=$(ip -o -4 route show to default | awk '{print $5}')
+        PUBLIC_IP=$(ip -o -4 addr show dev $PUBLIC_IFACE | awk '{print $4}' | cut -d/ -f1)
+    else
+        # FreeBSD - get public IP
+        PUBLIC_IP=$(ifconfig | grep -B1 "inet " | grep -v "inet 127" | grep -v "inet $NFSIP" | awk '$1=="inet" {print $2}' | head -n1)
+    fi
+    
+    if [ -z "$PUBLIC_IP" ]; then
+        echo "Warning: Could not determine public IP address, external access may not work"
+    else
+        echo "Public IP address for external access: $PUBLIC_IP"
     fi
 fi
 
@@ -67,32 +106,52 @@ if ! grep -q "^$NFSDIR" /etc/exports; then
 
     echo ""
     echo "Setting up NFS exports"
-    #
-    # Export the NFS server directory to the subnet so that all clients
-    # can mount it.  To do that, we need the subnet. Grab that from
-    # /etc/hosts, and assume a netmask of 255.255.255.0, which will be
-    # fine 99.9% of the time.
-    #
-    NFSIP=`grep -i $HOSTNAME-$NFSNETNAME /etc/hosts | awk '{print $1}'`
-    NFSNET=`echo $NFSIP | awk -F. '{printf "%s.%s.%s.0", $1, $2, $3}'`
 
     if [ "$OS" = "Linux" ]; then
-	echo "$NFSDIR $NFSNET/24(rw,sync,no_root_squash,no_subtree_check,fsid=0)" >> /etc/exports
+        # Internal access
+        echo "$NFSDIR $NFSNET/24(rw,sync,no_root_squash,no_subtree_check,fsid=0)" > /etc/exports
+        
+        # Add external access if enabled
+        if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+            # For each allowed network, add to exports
+            for network in $ALLOWED_NETWORKS; do
+                echo "$NFSDIR $network(rw,sync,no_subtree_check)" >> /etc/exports
+            done
+        fi
     else
-	echo "$NFSDIR -network $NFSNET -mask 255.255.255.0 -maproot=root -alldirs" >> /etc/exports
+        # FreeBSD exports
+        echo "$NFSDIR -network $NFSNET -mask 255.255.255.0 -maproot=root -alldirs" > /etc/exports
+        
+        # Add external access if enabled
+        if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+            for network in $ALLOWED_NETWORKS; do
+                NETADDR=$(echo $network | cut -d/ -f1)
+                NETMASK=$(cdr2mask $(echo $network | cut -d/ -f2))
+                echo "$NFSDIR -network $NETADDR -mask $NETMASK -maproot=root -alldirs" >> /etc/exports
+            done
+        fi
     fi
 
     if [ "$OS" = "Linux" ]; then
-	# Make sure we start RPCbind to listen on the right interfaces.
-	echo "OPTIONS=\"-l -h 127.0.0.1 -h $NFSIP\"" > /etc/default/rpcbind
+        # For internal access only
+        if [ "$EXTERNAL_ACCESS" = "no" ]; then
+            # Make sure we start RPCbind to listen on the right interfaces.
+            echo "OPTIONS=\"-l -h 127.0.0.1 -h $NFSIP\"" > /etc/default/rpcbind
+        else
+            # For external access, listen on all interfaces
+            echo "OPTIONS=\"-l\"" > /etc/default/rpcbind
+        fi
 
-	# We want to allow rpcinfo to operate from the clients.
-	sed -i.bak -e "s/^rpcbind/#rpcbind/" /etc/hosts.deny
+        # We want to allow rpcinfo to operate from the clients.
+        sed -i.bak -e "s/^rpcbind/#rpcbind/" /etc/hosts.deny
     else
-	# On FreeBSD we will start all the services manually
-	# But make sure the options are correct
-	cp -p /etc/rc.conf /etc/rc.conf.bak
-	cat <<EOF >> /etc/rc.conf
+        # On FreeBSD we will start all the services manually
+        # But make sure the options are correct
+        cp -p /etc/rc.conf /etc/rc.conf.bak
+        
+        if [ "$EXTERNAL_ACCESS" = "no" ]; then
+            # Internal access only
+            cat <<EOF >> /etc/rc.conf
 rpcbind_enable="NO"
 rpcbind_flags="-h $NFSIP"
 rpc_lockd_enable="NO"
@@ -105,8 +164,28 @@ nfs_server_enable="NO"
 nfs_server_flags="-u -t -h $NFSIP"
 nfs_reserved_port_only="YES"
 EOF
+        else
+            # External access - listen on all interfaces
+            cat <<EOF >> /etc/rc.conf
+rpcbind_enable="NO"
+rpc_lockd_enable="NO"
+rpc_statd_enable="NO"
+mountd_enable="NO"
+nfs_server_enable="NO"
+nfs_server_flags="-u -t"
+nfs_reserved_port_only="YES"
+EOF
+        fi
     fi
 fi
+
+# Define CIDR to netmask conversion function for FreeBSD
+cdr2mask() {
+    # Number of args to shift, 255..255, first non-255 byte, zeroes
+    set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+    [ $1 -gt 1 ] && shift $1 || shift
+    echo ${1-0}.${2-0}.${3-0}.${4-0}
+}
 
 #
 # Create prepare hook to remove our customizations before we take the
@@ -116,7 +195,7 @@ fi
 #
 if [ ! -e $HOOKNAME ]; then
     if [ "$OS" = "Linux" ]; then
-	cat <<EOFL > $HOOKNAME
+        cat <<EOFL > $HOOKNAME
 sed -i.bak -e '/^\\$NFSDIR/d' /etc/exports
 sed -i.bak -e "s/^#rpcbind/rpcbind/" /etc/hosts.deny
 echo "OPTIONS=\"-l -h 127.0.0.1\"" > /etc/default/rpcbind
@@ -124,7 +203,7 @@ rm -f $HOOKNAME
 exit 0
 EOFL
     else
-	cat <<EOFB > $HOOKNAME
+        cat <<EOFB > $HOOKNAME
 sed -i.bak -e '/^\\$NFSDIR/d' /etc/exports
 # stopping services when making a snapshot might not be a
 # good idea; i.e., if one of the services hangs
@@ -154,13 +233,46 @@ fi
 echo "Starting NFS services"
 if [ "$OS" = "Linux" ]; then
     service nfs-kernel-server start
+    
+    # Configure firewall if external access is enabled
+    if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+        echo "Configuring firewall for external NFS access"
+        # Reset UFW to default
+        ufw --force reset
+        
+        # Allow NFS-related ports
+        ufw allow ssh
+        ufw allow nfs
+        ufw allow 111/tcp
+        ufw allow 111/udp
+        ufw allow 2049/tcp
+        ufw allow 2049/udp
+        ufw allow 32765:32769/tcp
+        ufw allow 32765:32769/udp
+        
+        # Enable firewall
+        ufw --force enable
+    fi
 else
     # nfsd starts rpcbind and mountd
     /etc/rc.d/nfsd onestart
     /etc/rc.d/statd onestart
     /etc/rc.d/lockd onestart
+    
+    # For FreeBSD, configure firewall if needed
+    if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+        echo "Note: Manual firewall configuration may be needed for FreeBSD"
+    fi
 fi
 
 # Give it time to start-up
 sleep 5
 
+if [ "$EXTERNAL_ACCESS" = "yes" ]; then
+    echo ""
+    echo "======================= EXTERNAL ACCESS INFO ========================"
+    echo "NFS server is configured for external access!"
+    echo "To mount from another experiment, use:"
+    echo "  sudo mount -t nfs $PUBLIC_IP:$NFSDIR /mnt"
+    echo "===================================================================="
+fi
--- profile.py
+++ profile.py
@@ -1,10 +1,12 @@
-"""This profile sets up a simple NFS server and a network of clients. The NFS server uses 
-*temporary* storage on one of your nodes, the contents will be lost when you terminate your
-experiment. We have a different profile available if you need your NFS server data to
-persist after your experiment is terminated. 
+"""This profile sets up an NFS server that can be accessed from other experiments. The NFS server 
+uses persistent storage that will remain available even after this experiment is terminated.
 
 Instructions:
-Click on any node in the topology and choose the `shell` menu item. Your shared NFS directory is mounted at `/nfs` on all nodes."""
+- The NFS server will have both an internal interface (for clients in this experiment) 
+  and a public interface (for clients in other experiments)
+- Your shared NFS directory is mounted at `/nfs` on all nodes
+- To access from other experiments, use the public IP address of the NFS server
+- The server hostname 'nfs' will be resolvable within CloudLab"""
 
 # Import the Portal object.
 import geni.portal as portal
@@ -58,6 +60,18 @@ pc.defineParameter("nfsSize", "Size of NFS Storage",
                    portal.ParameterType.STRING, "200GB",
                    longDescription="Size of disk partition to allocate on NFS server")
 
+pc.defineParameter("usePersistentStorage", "Use persistent storage",
+                   portal.ParameterType.BOOLEAN, True,
+                   longDescription="Check to use persistent storage that will remain available after experiment termination")
+
+pc.defineParameter("allowExternalAccess", "Allow external NFS access",
+                   portal.ParameterType.BOOLEAN, True,
+                   longDescription="Check to allow NFS access from other experiments")
+
+pc.defineParameter("externalNetworks", "Networks allowed for external access",
+                   portal.ParameterType.STRING, "10.0.0.0/8 172.16.0.0/12 192.168.0.0/16",
+                   longDescription="Space-separated list of networks in CIDR notation allowed to access NFS")
+
 # Always need this when using parameters
 params = pc.bindParameters()
 
@@ -70,13 +84,32 @@ nfsLan.link_multiplexing = True
 # The NFS server.
 nfsServer = request.RawPC(nfsServerName)
 nfsServer.disk_image = params.osServerImage
-# Attach server to lan.
+
+# Add a public interface to the NFS server if external access is enabled
+if params.allowExternalAccess:
+    nfsServer.routable_control_ip = True
+
+# Attach server to lan for internal access
 nfsLan.addInterface(nfsServer.addInterface())
-# Storage file system goes into a local (ephemeral) blockstore.
-nfsBS = nfsServer.Blockstore("nfsBS", nfsDirectory)
-nfsBS.size = params.nfsSize
+
+# Storage: either persistent or ephemeral depending on user choice
+if params.usePersistentStorage:
+    # Use a persistent dataset
+    nfsBS = nfsServer.Blockstore("nfsBS", nfsDirectory)
+    nfsBS.size = params.nfsSize
+    nfsBS.persistent = True
+else:
+    # Use a temporary blockstore (original behavior)
+    nfsBS = nfsServer.Blockstore("nfsBS", nfsDirectory)
+    nfsBS.size = params.nfsSize
+
+# Pass the external access parameters to the server setup script
+nfsServerCmd = "sudo /bin/bash /local/repository/nfs-server.sh"
+if params.allowExternalAccess:
+    nfsServerCmd += " --external-access=yes --allowed-networks='" + params.externalNetworks + "'"
+
 # Initialization script for the server
-nfsServer.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-server.sh"))
+nfsServer.addService(pg.Execute(shell="sh", command=nfsServerCmd))
 
 # The NFS clients, also attached to the NFS lan.
 for i in range(1, params.clientCount+1):
